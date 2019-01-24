4cdfd255ecff79091008e1441fd8e2b560208d69
Kenji Funaoka <kfunaoka@gmail.com>
2019-01-24 13:03:43 +0900

    Remove enablePlannerDynamicSwitch from lane_select_core.cpp

--- ros/src/computing/planning/mission/packages/lane_planner/nodes/lane_select/lane_select_core.cpp
+++ ros/src/computing/planning/mission/packages/lane_planner/nodes/lane_select/lane_select_core.cpp
@@ -54,21 +54,10 @@ void LaneSelectNode::initForROS()
   sub5_ = nh_.subscribe("/config/lane_select", 1, &LaneSelectNode::callbackFromConfig, this);
   sub6_ = nh_.subscribe("/decisionmaker/states", 1, &LaneSelectNode::callbackFromStates, this);
 
-  bool enablePlannerDynamicSwitch;
-  private_nh_.param<bool>("enablePlannerDynamicSwitch", enablePlannerDynamicSwitch, false);
   // setup publisher
 
   pub1_ = nh_.advertise<autoware_msgs::Lane>("base_waypoints", 1);
-
-  if (enablePlannerDynamicSwitch)
-  {
-    pub2_ = nh_.advertise<std_msgs::Int32>("/astar/closest_waypoint", 1);
-  }
-  else
-  {
-    pub2_ = nh_.advertise<std_msgs::Int32>("closest_waypoint", 1);
-  }
-
+  pub2_ = nh_.advertise<std_msgs::Int32>("closest_waypoint", 1);
   pub3_ = nh_.advertise<std_msgs::Int32>("change_flag", 1);
   pub4_ = nh_.advertise<std_msgs::Int32>("/current_lane_id",1);
 
